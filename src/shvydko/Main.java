ae23d77b8b3b662be0d4ee38d76876c45ebf3b2f
Nanakay62 <dic20016@byui.edu>
2021-05-22 20:59:17 +0000

    Lines added to extend greetings depending on time of day. Added a space to line 8 to make it more legible.

--- src/shvydko/Main.java
+++ src/shvydko/Main.java
@@ -5,6 +5,15 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.print("Hello,my name is Nastya");
-	System.out.println("Hello Nastya. My name is Jurgen!");
+	System.out.println(" Hello Nastya. My name is Jurgen!");
+
+        int time = 22;
+        if (time < 10) {
+            System.out.println("Good morning, Anastasiya. I hope you slept well");
+        } else if (time < 20) {
+            System.out.println("Good day, Nastya. How is your day going?");
+        }  else {
+            System.out.println("Good evening, Jurgen and Nastya. Have goodnight, both of you.");
+        }
     }
 }
